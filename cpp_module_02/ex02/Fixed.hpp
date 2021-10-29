26b911b31bf4d0d56b890b19d79eab050eded20a
xueming-wang <wxueming0614@hotmail.com>
2021-10-29 20:00:37 +0200

    push

--- cpp_module_02/ex02/Fixed.hpp
+++ cpp_module_02/ex02/Fixed.hpp
@@ -6,7 +6,36 @@
 /*   By: xuwang <xuwang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 18:09:17 by xuwang            #+#    #+#             */
-/*   Updated: 2021/10/28 18:09:18 by xuwang           ###   ########.fr       */
+/*   Updated: 2021/10/29 19:59:45 by xuwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed {
+
+    private:
+
+        int valeur;
+        const static int bits = 8;
+       
+    public:
+
+        Fixed(void);
+        Fixed(const int i);
+        Fixed(const float f); 
+        Fixed(const Fixed &a); // constructeur par copie 
+        ~Fixed(void);
+    
+    Fixed &operator = (const Fixed &b);
+    float toFloat(void) const; //qui convertit un nombres à point fixe en float.
+    int toInt(void) const; //un nombres à point fixe en int.
+    int getRawBits(void)const;  //它返回定点数的原始值。
+    void setRawBits(int const raw); // qui set la valeur du nombre à point fixe.
+};
+
+#endif
--- cpp_module_02/ex02/main.cpp
+++ cpp_module_02/ex02/main.cpp
@@ -6,7 +6,29 @@
 /*   By: xuwang <xuwang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 18:09:19 by xuwang            #+#    #+#             */
-/*   Updated: 2021/10/28 18:09:20 by xuwang           ###   ########.fr       */
+/*   Updated: 2021/10/29 19:49:26 by xuwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include "Fixed.hpp"
+
+int main(void) {
+    
+    Fixed a;
+    Fixed const b(10); 
+    Fixed const c(42.42f); 
+    Fixed const d(b);
+    a = Fixed(1234.4321f);
+    
+    std::cout << "a is " << a << std::endl;
+    std::cout << "b is " << b << std::endl;
+    std::cout << "c is " << c << std::endl;
+    std::cout << "d is " << d << std::endl;
+    std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+    std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+    std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+    std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
+    return 0; 
+}
