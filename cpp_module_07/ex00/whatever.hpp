cefe79dcb98081b9144d93a4cb0731e56b5b734b
xueming-wang <wxueming0614@hotmail.com>
2021-11-25 19:13:21 +0100

    xuwang

--- cpp_module_07/ex00/whatever.hpp
+++ cpp_module_07/ex00/whatever.hpp
@@ -6,7 +6,35 @@
 /*   By: xuwang <xuwang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 12:31:11 by xuwang            #+#    #+#             */
-/*   Updated: 2021/11/25 12:31:12 by xuwang           ###   ########.fr       */
+/*   Updated: 2021/11/25 19:08:16 by xuwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+#include <iostream>
+template <typename T> T const &max(T const &a, T const &b)
+{
+    if (a > b)
+        return a;
+    else
+        return b;
+}
+
+template <typename T> T const &min(T const &a, T const &b)
+{
+    if (a < b)
+        return a;
+    else
+        return b;
+}
+
+template < typename T > void swap(T &a, T &b)
+{
+    T tmp = a;
+    a = b;
+    b = tmp;
+}
+
+#endif
--- cpp_module_07/ex01/iter.hpp
+++ cpp_module_07/ex01/iter.hpp
@@ -6,7 +6,19 @@
 /*   By: xuwang <xuwang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 12:31:14 by xuwang            #+#    #+#             */
-/*   Updated: 2021/11/25 12:31:15 by xuwang           ###   ########.fr       */
+/*   Updated: 2021/11/25 19:13:12 by xuwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef ITER_HPP
+#define TIER_HPP
+
+#include <iostream>
+
+tmplate T <typename T>
+void iter(T *arry, T &size, )
+
+
+
+
+#endif
